1a7643abfd8b3e567e647d0170c038b047dce290
John Paul <greerjohnpaul@gmail.com>
2018-07-18 10:27:22 -0500

    added methods and finished tasks 1-4

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -6,7 +6,19 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the name of your lemonade stand?");
+            Console.ReadLine();
+            Console.WriteLine("How many cups of lemonade will you sell?");
+            Console.ReadLine();
+            Console.WriteLine("How much money are you charging per cup?");
+            Console.ReadLine();
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
