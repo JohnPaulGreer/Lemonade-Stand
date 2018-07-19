26d62104acb5c4665f40c7a3fe0832cee4b45c6e
John Paul <greerjohnpaul@gmail.com>
2018-07-19 09:56:34 -0500

    Finished making a LemonadeStandclass, and added methods to that class

---
+++ LemonadeStand/LemonadeStand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace LemonadeStand
+{
+    class LemonadeStand
+    {
+		public string name  { get; set; }
+        public int numOfCups { get; set; }
+        public double pricePerCup { get; set; }
+        public double costPerCup { get; set; }
+
+        public string GetUserStringResponse(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(response))
+            {
+                Console.WriteLine("Please enter an answer. " + question);
+                response = Console.ReadLine();
+            }
+
+            return response;
+        }
+
+        public int GetUserInegerInput(string question)
+        {
+            Console.WriteLine(question);
+            int response;
+            while (!int.TryParse(Console.ReadLine(), out response))
+            {
+                Console.WriteLine("Unable to determine response. " + question);
+            }
+
+            return response;
+        }
+
+        public double GetUserDoubleInput(string question)
+        {
+            Console.WriteLine(question);
+            double response;
+            while (!double.TryParse(Console.ReadLine(), out response))
+            {
+                Console.WriteLine("Unable to determine response. " + question);
+            }
+
+            return response;
+        }
+    }
+}
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -4,69 +4,32 @@ namespace LemonadeStand
 {
     class Program
     {
-        static string GetUserStringResponse(string question)
-        {
-            Console.WriteLine(question);
-            string response = Console.ReadLine();
-
-            while (string.IsNullOrWhiteSpace(response))
-            {
-                Console.WriteLine("Please enter an answer. " + question);
-                response = Console.ReadLine();
-            }
-
-            return response;
-        }
-
-        static int GetUserInegerInput(string question)
-        {
-            Console.WriteLine(question);
-            int response;
-            while (!int.TryParse(Console.ReadLine(), out response))
-            {
-                Console.WriteLine("Unable to determine response. " + question);
-            }
-
-            return response;
-        }
-
-        static double GetUserDoubleInput(string question)
-        {
-            Console.WriteLine(question);
-            double response;
-            while (!double.TryParse(Console.ReadLine(), out response))
-            {
-                Console.WriteLine("Unable to determine response. " + question);
-            }
-
-            return response;
-        }
-
         static void Main(string[] args)
         {
+            LemonadeStand lemonadeStand = new LemonadeStand();
             Console.WriteLine("Welcome!");
-            Console.WriteLine();
-            string name = GetUserStringResponse("What is the name of your lemonade stand?");
-            int numOfCups = GetUserInegerInput("How many cups will you sell?");
-            double pricePerCup = GetUserDoubleInput("How much will you charge per cup?");
-            double costPerCup = GetUserDoubleInput("How much will you spend to produce the lemonade?");
+
+            lemonadeStand.name = lemonadeStand.GetUserStringResponse("What is the name of your lemonade stand?");
+            lemonadeStand.numOfCups = lemonadeStand.GetUserInegerInput("How many cups will you sell?");
+            lemonadeStand.pricePerCup = lemonadeStand.GetUserDoubleInput("How much will you charge per cup?");
+            lemonadeStand.costPerCup = lemonadeStand.GetUserDoubleInput("How much will you spend to produce the lemonade?");
 
             Console.WriteLine();
             Console.WriteLine("After calculating all of your inputs...");
             Console.WriteLine("Recap:");
             Console.WriteLine();
-            Console.WriteLine(name + " sounds like a good name for your lemonade stand.");
+            Console.WriteLine(lemonadeStand.name + " sounds like a good name for your lemonade stand.");
             Console.WriteLine();
-            Console.WriteLine("If you are going to sell " + numOfCups + " cups of lemonade");
-            Console.WriteLine("and you are charging $" + pricePerCup + " ");
-            Console.WriteLine("and it costs you $" + costPerCup + " per cup of lemonade.");
+            Console.WriteLine("If you are going to sell " + lemonadeStand.numOfCups + " cups of lemonade");
+            Console.WriteLine("and you are charging $" + lemonadeStand.pricePerCup + " ");
+            Console.WriteLine("and it costs you $" + lemonadeStand.costPerCup + " per cup of lemonade.");
             Console.WriteLine();
 
-            var revenue = numOfCups * pricePerCup;
+            var revenue = lemonadeStand.numOfCups * lemonadeStand.pricePerCup;
             Console.WriteLine("Then the total amount of money the lemonade stand would bring in is $" + revenue + " ");
 
-            var net = revenue - costPerCup * numOfCups;
-            Console.WriteLine("and total profit for the lemonade stand per " + numOfCups + " cups is $" + net + ".");
+            var net = revenue - lemonadeStand.costPerCup * lemonadeStand.numOfCups;
+            Console.WriteLine("and total profit for the lemonade stand per " + lemonadeStand.numOfCups + " cups is $" + net + ".");
 
             Console.ReadLine();
         }
