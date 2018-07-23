76471500edb2069178eb9160767eb359499ac8b4
John Paul <greerjohnpaul@gmail.com>
2018-07-23 12:10:32 -0500

    added a LemonadeStandCorporation class.

---
+++ LemonadeStand/LemonadeStandCorporation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LemonadeStand
+{
+    class LemonadeStandCorporation
+    {
+        public string Name { get; set; }
+        public List<LemonadeStand> Locations { get; set; } = new List<LemonadeStand>();
+
+        public double GetTotalRevenue()
+        {
+            double grandTotal = 0;
+            foreach(LemonadeStand location in Locations)
+            {
+                grandTotal += location.totalRevenue();
+            }
+            return grandTotal;
+        }
+    }
+}
