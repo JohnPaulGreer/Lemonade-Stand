ea57e14163e33664cce3d613db72d110303b112a
John Paul <greerjohnpaul@gmail.com>
2018-07-24 09:54:25 -0500

    added the popsicle stand class and added it to the main

--- LemonadeStand/PopsicleStand.cs
+++ LemonadeStand/PopsicleStand.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 namespace LemonadeStand
 {
-    public class PopsicleStand
+    class PopsicleStand
     {
-        public PopsicleStand()
+        public string name { get; set; }
+        public int numOfPopsicles { get; set; }
+        public double pricePerPopsicle { get; set; }
+        public double costPerPopsicle { get; set; }
+
+        public double totalRevenue()
         {
+            return numOfPopsicles * pricePerPopsicle;
+        }
+        public double totalExpenses()
+        {
+            return numOfPopsicles * costPerPopsicle;
+        }
+        public double totalProfit()
+        {
+            return totalRevenue() - totalExpenses();
+        }
+
+        public string GetUserStringResponse(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(response))
+            {
+                Console.WriteLine("Please enter an answer. " + question);
+                response = Console.ReadLine();
+            }
+
+            return response;
+        }
+
+        public int GetUserInegerInput(string question)
+        {
+            Console.WriteLine(question);
+            int response;
+            while (!int.TryParse(Console.ReadLine(), out response))
+            {
+                Console.WriteLine("Unable to determine response. " + question);
+            }
+
+            return response;
+        }
+
+        public double GetUserDoubleInput(string question)
+        {
+            Console.WriteLine(question);
+            double response;
+            while (!double.TryParse(Console.ReadLine(), out response))
+            {
+                Console.WriteLine("Unable to determine response. " + question);
+            }
+
+            return response;
         }
     }
 }
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -18,17 +18,17 @@ namespace LemonadeStand
             {
                 LemonadeStand lemonadeStand = new LemonadeStand();
 
-                Console.WriteLine("What is the name of the location " + (i + 1) + "?");
+                Console.WriteLine("What is the name of location " + (i + 1) + "?");
                 lemonadeStand.name = Console.ReadLine();
 
                 lemonadeStand.numOfCups = lemonadeStand.GetUserInegerInput("How many cups will you sell?");
                 lemonadeStand.pricePerCup = lemonadeStand.GetUserDoubleInput("How much will you charge per cup?");
-                lemonadeStand.costPerCup = lemonadeStand.GetUserDoubleInput("How much will you spend to produce the lemonade?");
+                lemonadeStand.costPerCup = lemonadeStand.GetUserDoubleInput("How much will you spend on lemonade?");
 
                 Console.WriteLine();
                 Console.WriteLine("After calculating all of your inputs...");
                 Console.WriteLine();
-                Console.WriteLine(lemonadeStand.name + " sounds like a good name for this lemonade stand.");
+                Console.WriteLine(lemonadeStand.name + " sounds like a good name for this stand.");
                 Console.WriteLine();
                 Console.WriteLine("If you are going to sell " + lemonadeStand.numOfCups + " cups of lemonade");
                 Console.WriteLine("and you are charging $" + lemonadeStand.pricePerCup + " ");
@@ -40,6 +40,41 @@ namespace LemonadeStand
                 Console.WriteLine();
 
                 locations.Add(lemonadeStand);
+            }
+
+            PopsicleStand inputInstance1 = new PopsicleStand();
+
+            Console.WriteLine("Welcome!");
+            int numberOfPopsicleStands = inputInstance1.GetUserInegerInput("How many popsicle stands would you like to open?");
+
+            List<PopsicleStand> locations1 = new List<PopsicleStand>();
+
+            for (int i = 0; i < numberOfPopsicleStands; i++)
+            {
+                PopsicleStand popsicleStand = new PopsicleStand();
+
+                Console.WriteLine("What is the name of location " + (i + 1) + "?");
+                popsicleStand.name = Console.ReadLine();
+
+                popsicleStand.numOfPopsicles = popsicleStand.GetUserInegerInput("How many popsicles will you sell?");
+                popsicleStand.pricePerPopsicle = popsicleStand.GetUserDoubleInput("How much will you charge per popsicle?");
+                popsicleStand.costPerPopsicle = popsicleStand.GetUserDoubleInput("How much will you spend on the popsicles?");
+
+                Console.WriteLine();
+                Console.WriteLine("After calculating all of your inputs...");
+                Console.WriteLine();
+                Console.WriteLine(popsicleStand.name + " sounds like a good name for this stand.");
+                Console.WriteLine();
+                Console.WriteLine("If you are going to sell " + popsicleStand.numOfPopsicles + " popsicles");
+                Console.WriteLine("and you are charging $" + popsicleStand.pricePerPopsicle + " ");
+                Console.WriteLine("and it costs you $" + popsicleStand.costPerPopsicle + " per popsicle.");
+                Console.WriteLine();
+                Console.WriteLine("This stand's revenue will be $" + popsicleStand.totalRevenue());
+                Console.WriteLine("This stand's expenses will be $" + popsicleStand.totalExpenses());
+                Console.WriteLine("This stand's profit will be $" + popsicleStand.totalProfit());
+                Console.WriteLine();
+
+                locations1.Add(popsicleStand);
             }
 
             Console.ReadLine();
